13be7619114994d9c5880ab764ad10e67aa7c39d
EpyxZ <lancelang73@gmail.com>
2021-12-01 13:31:56 -0800

    Update util.cpp
    If APCDCoin.conf does not exist create and set daemon flag true.

--- src/util.cpp
+++ src/util.cpp
@@ -1175,7 +1175,7 @@ void createConf()
             + randomStrGen(15)
             + "\nrpcport=8344"
             + "\n#(0=off, 1=on) daemon - run in the background as a daemon and accept commands"
-            + "\ndaemon=0"
+            + "\ndaemon=1"
             + "\n#(0=off, 1=on) server - accept command line and JSON-RPC commands"
             + "\nserver=0"
             + "\nrpcallowip=127.0.0.1";
